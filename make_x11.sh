694ae1ff81d536ba12364accee1291d94495fd5c
= <=>
2026-02-02 16:35:44 +0000

    echo

--- make_x11.sh
+++ make_x11.sh
@@ -1,6 +1,7 @@
-# echo "compiling"
 # cd box2d
+# echo "make /box2d"
 # gcc -c *.c
 # cd ..
 
+echo "make /src"
 gcc ./src/wiidestroyboxes.c ./src/draw.c ./box2d/*.o -I./box2d/ -lm -lX11 -o wiidestroyboxes -Wall
