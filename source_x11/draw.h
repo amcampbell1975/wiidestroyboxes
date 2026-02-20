6f1c2dfec5cc75ecaf3b8203182629c00989e064
= <=>
2026-02-20 11:47:05 +0000

    Removed x11

--- source_x11/draw.h
+++
@@ -1,7 +0,0 @@
-#include <X11/Xlib.h>
-
-void draw_rotated_rect(Display *d, Window w, GC gc,
-                       int x, int y, int width, int height,
-                       float angle_deg);
-
-
